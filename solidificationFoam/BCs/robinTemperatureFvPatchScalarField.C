7edf03fb40ea072d1adbec531043244f37aab5e5
colemaj <colemaj@purdue.edu>
2018-09-20 08:35:24 -0400

    Initial contributions of the binary solidification solver and the SMACS numerical benchmark tutorial case.

---
+++ solidificationFoam/BCs/robinTemperatureFvPatchScalarField.C
@@ -0,0 +1,181 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+
+#include "robinTemperatureFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::robinTemperatureFvPatchScalarField::robinTemperatureFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mixedFvPatchScalarField(p, iF),
+    Tinf_(p.size(), 0.0),
+    h_(p.size(), 0.0)
+{
+    refValue() = 0.0;
+    refGrad() = 0.0;
+    valueFraction() = 0.0;
+}
+
+
+Foam::robinTemperatureFvPatchScalarField::robinTemperatureFvPatchScalarField
+(
+    const robinTemperatureFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    mixedFvPatchScalarField(ptf, p, iF, mapper),
+    Tinf_(ptf.Tinf_, mapper),
+    h_(ptf.h_, mapper)
+{}
+
+
+Foam::robinTemperatureFvPatchScalarField::robinTemperatureFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    mixedFvPatchScalarField(p, iF),
+    Tinf_("Tinf", dict, p.size()),
+    h_("h", dict, p.size())
+{
+    refValue() = Tinf_;
+    refGrad() = 0.0;
+    valueFraction() = 0.0;
+
+    if (dict.found("value"))
+    {
+        fvPatchField<scalar>::operator=
+        (
+            scalarField("value", dict, p.size())
+        );
+    }
+    else
+    {
+        evaluate();
+    }
+}
+
+
+Foam::robinTemperatureFvPatchScalarField::robinTemperatureFvPatchScalarField
+(
+    const robinTemperatureFvPatchScalarField& tppsf
+)
+:
+    mixedFvPatchScalarField(tppsf),
+    Tinf_(tppsf.Tinf_),
+    h_(tppsf.h_)
+{}
+
+
+Foam::robinTemperatureFvPatchScalarField::robinTemperatureFvPatchScalarField
+(
+    const robinTemperatureFvPatchScalarField& tppsf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mixedFvPatchScalarField(tppsf, iF),
+    Tinf_(tppsf.Tinf_),
+    h_(tppsf.h_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::robinTemperatureFvPatchScalarField::autoMap
+(
+    const fvPatchFieldMapper& m
+)
+{
+    scalarField::autoMap(m);
+    Tinf_.autoMap(m);
+    h_.autoMap(m);
+}
+
+
+void Foam::robinTemperatureFvPatchScalarField::rmap
+(
+    const fvPatchScalarField& ptf,
+    const labelList& addr
+)
+{
+    mixedFvPatchScalarField::rmap(ptf, addr);
+
+    const robinTemperatureFvPatchScalarField& tiptf =
+        refCast<const robinTemperatureFvPatchScalarField>(ptf);
+
+    Tinf_.rmap(tiptf.Tinf_, addr);
+    h_.rmap(tiptf.h_, addr);
+}
+
+
+void Foam::robinTemperatureFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+    
+    const scalarField& k_ =
+        patch().lookupPatchField<volScalarField, scalar>("kappa");  
+        
+    valueFraction() =
+        1.0/
+        (
+            1.0
+          + k_*patch().deltaCoeffs()/h_
+        );
+
+    mixedFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::robinTemperatureFvPatchScalarField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+    Tinf_.writeEntry("Tinf", os);
+    h_.writeEntry("h", os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        robinTemperatureFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
---
+++ solidificationFoam/solidificationSystem/solidificationSystem.C
@@ -0,0 +1,190 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2014-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "solidificationSystem.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(solidificationSystem, 0);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solidificationSystem::solidificationSystem
+(
+    const volVectorField& U
+)
+:
+    IOdictionary
+    (
+        IOobject
+        (
+            "transportProperties",
+            U.time().constant(),
+            U.db(),
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+        )
+    ),
+    twoPhaseMixture(U.mesh(), *this),
+    
+    rho1_
+    (
+        "rho", 
+        dimensionSet(1, -3, 0, 0, 0), 
+        subDict(phase1Name_)
+    ),
+    rho2_
+    (
+        "rho", 
+        dimensionSet(1, -3, 0, 0, 0), 
+        subDict(phase2Name_)
+    ),
+    
+    Cp1_
+    (
+        "Cp", 
+        dimensionSet(0, 2, -2, -1, 0), 
+        subDict(phase1Name_)
+    ),
+    Cp2_
+    (
+        "Cp", 
+        dimensionSet(0, 2, -2, -1, 0), 
+        subDict(phase2Name_)
+    ), 
+       
+    kappa1_
+    (
+        "kappa", 
+        dimensionSet(1, 1, -3, -1, 0), 
+        subDict(phase1Name_)
+    ),
+    kappa2_
+    (
+        "kappa", 
+        dimensionSet(1, 1, -3, -1, 0), 
+        subDict(phase2Name_)
+    ),   
+     
+    mu1_
+    (
+        "mu", 
+        dimensionSet(1, -1, -1, 0, 0), 
+        subDict(phase1Name_)
+    ),
+    mu2_
+    (
+        "mu", 
+        dimensionSet(1, -1, -1, 0, 0),
+        subDict(phase2Name_)
+    ),
+    
+    D1_
+    (
+        "D", 
+        dimensionSet(0, 2, -1, 0, 0), 
+        subDict(phase1Name_)
+    ),
+    D2_
+    (
+        "D", 
+        dimensionSet(0, 2, -1, 0, 0),
+        subDict(phase2Name_)
+    ),
+    DAS_
+    (
+        "DAS", 
+        dimensionSet(0, 1, 0, 0, 0),
+        subDict(phase1Name_)
+    ),
+    betaT_
+    (
+        "betaT", 
+        dimensionSet(0, 0, 0, -1, 0), 
+        subDict(phase2Name_)
+    ),
+    betaC_
+    (
+        "betaC", 
+        dimensionSet(0, 0, 0, 0, 0), 
+        subDict(phase2Name_)
+    ),
+    TRef_
+    (
+        "TRef", 
+        dimensionSet(0, 0, 0, 1, 0), 
+        subDict(phase2Name_)
+    ),
+    CRef_
+    (
+        "CRef", 
+        dimensionSet(0, 0, 0, 0, 0), 
+        subDict(phase2Name_)
+    ),
+           
+    U_(U)
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::solidificationSystem::read()
+{
+    const dictionary& phaseDict1_(subDict(phase1Name_));
+    const dictionary& phaseDict2_(subDict(phase2Name_));
+    
+    rho1_ = phaseDict1_.lookup("rho");
+    rho2_ = phaseDict2_.lookup("rho");
+    
+    Cp1_ = phaseDict1_.lookup("Cp");
+    Cp2_ = phaseDict2_.lookup("Cp");
+    
+    kappa1_ = phaseDict1_.lookup("kappa");
+    kappa2_ = phaseDict2_.lookup("kappa");
+    
+    mu1_ = phaseDict1_.lookup("mu");
+    mu2_ = phaseDict2_.lookup("mu");
+    
+    D1_ = phaseDict1_.lookup("D");
+    D2_ = phaseDict2_.lookup("D");
+    
+    DAS_ = phaseDict1_.lookup("DAS");
+    
+    betaT_ = phaseDict2_.lookup("betaT");
+    betaC_ = phaseDict2_.lookup("betaC");    
+    
+    TRef_ = phaseDict2_.lookup("TRef");
+    CRef_ = phaseDict2_.lookup("CRef");
+         
+    return true;
+}
+
+
+// ************************************************************************* //
